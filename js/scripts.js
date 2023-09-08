e7658988b245d57c2162cf81759acf14e7ce303f
aleanrocha <ale.oficial.me@gmail.com>
2023-09-08 19:16:43 -0300

    finalizando js

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach((entry) => {
     if(!entry.isIntersecting) return
     const img = entry.target
-    img.src = img.src.replace("&w=10&", "&w=1500&")
+    img.src = img.src.replace("&w=10&", "&w=1000&")
     observer.unobserve(img)
   })
 })
